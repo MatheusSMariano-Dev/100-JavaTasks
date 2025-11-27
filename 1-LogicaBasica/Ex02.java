d5c19c2312535faf9a01da49c324f78a3b1e7192
Matheus Mariano <amatheus300@hotmail.com>
2025-11-27 00:45:50 -0300

    Segunda task

--- 1-LogicaBasica/Ex02.java
+++ 1-LogicaBasica/Ex02.java
@@ -1,5 +1,10 @@
 public class Ex02 {
     public static void main(String[] args) {
 
+        int number1 = 100;
+        int number2 = 300;
+        String Resultado = number1 + number2 + "";
+        System.out.println("O resultado da soma é : " + Resultado);
+
     }
 }
