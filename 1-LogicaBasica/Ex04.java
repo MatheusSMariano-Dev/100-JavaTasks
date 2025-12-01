b08f083257d72489a5c31efe7fb03fc35b0c02ae
Matheus Mariano <amatheus300@hotmail.com>
2025-11-30 23:56:35 -0300

    Quarto do dia da Task

--- 1-LogicaBasica/Ex04.java
+++ 1-LogicaBasica/Ex04.java
@@ -1,5 +1,8 @@
 public class Ex04 {
     public static void main(String[] args) {
 
+        int number1 = 1900;
+        int number2 = 300;
+        System.out.println("O resultado da multiplicacao é : " + (number1 * number2));
     }
 }
