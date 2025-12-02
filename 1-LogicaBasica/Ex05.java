26898c31b8871beb43324b942902d70a9959df3f
Matheus Mariano <amatheus300@hotmail.com>
2025-12-02 01:13:27 -0300

    SextoDiaDeTask

--- 1-LogicaBasica/Ex05.java
+++ 1-LogicaBasica/Ex05.java
@@ -1,5 +1,9 @@
 public class Ex05 {
     public static void main(String[] args) {
 
+        int NB1 = 10000;
+        int NB2 = 300;
+        double Resultado = (double) NB1 / NB2 ;
+        System.out.println("O resultado da soma é : " + Resultado);
     }
 }
