3a61e74d6473d27e96719d5d5761fb9ca15ce4c5
Matheus Mariano <amatheus300@hotmail.com>
2025-12-02 22:51:42 -0300

    SetimoDiaDaTask

--- 1-LogicaBasica/Ex06.java
+++ 1-LogicaBasica/Ex06.java
@@ -1,5 +1,12 @@
 public class Ex06 {
     public static void main(String[] args) {
 
+        double nb1 = 5000;
+        double nb2 = 3000;
+        double nb3 = 1000;
+
+        double media = (nb1 + nb2 + nb3 ) / 3;
+
+        System.out.println("O resultado da media é : " + media);
     }
 }
