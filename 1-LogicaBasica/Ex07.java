c341772788c3c411dbc6a30abfba75ce386623d1
Matheus Mariano <amatheus300@hotmail.com>
2025-12-04 01:58:23 -0300

    Task7

--- 1-LogicaBasica/Ex07.java
+++ 1-LogicaBasica/Ex07.java
@@ -1,5 +1,22 @@
-public class Ex07 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Ex07 {
+
+    public static class ParOuImpar {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite um número: ");
+            int numero = scanner.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println("O número " + numero + " é Par");
+            } else {
+                System.out.println("O número " + numero + " é Impar");
+            }
+            scanner.close();
+        }
     }
+
 }
