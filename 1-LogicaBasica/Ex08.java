3391f84dbbd3779a54fc74f390ca89789edae870
Matheus Mariano <amatheus300@hotmail.com>
2025-12-07 12:17:06 -0300

    OitavoDiaDeTask

--- 1-LogicaBasica/Ex08.java
+++ 1-LogicaBasica/Ex08.java
@@ -1,5 +1,17 @@
-public class Ex08 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Ex08 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            if (n > 0) {
+                System.out.println("Positivo");
+            } else if (n < 0) {
+                System.out.println("Negativo");
+            } else {
+                System.out.println("Zero");
+            }
+        }
     }
-}
