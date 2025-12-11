0900dc773057dac6fc75e24133701cac5a69fbdf
Matheus Mariano <amatheus300@hotmail.com>
2025-12-11 20:28:07 -0300

    Task9

--- 1-LogicaBasica/Ex09.java
+++ 1-LogicaBasica/Ex09.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Ex09 {
-    public static void main(String[] args) {
+
+        public class Main {
+            public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+
+                int ano = input.nextInt();
+
+                if (ano % 400 == 0) {
+                    System.out.println("Ano bissexto");
+                } else if (ano % 4 == 0 && ano % 100 != 0) {
+                    System.out.println("Ano bissexto");
+                } else {
+                    System.out.println("Não é bissexto");
+                }
+            }
+        }
+
 
     }
-}
