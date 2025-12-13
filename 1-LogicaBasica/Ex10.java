59f8a92e3f05e771faf3aa50e9b67dbed453e77d
Matheus Mariano <amatheus300@hotmail.com>
2025-12-12 23:46:09 -0300

    Task10

--- 1-LogicaBasica/Ex10.java
+++ 1-LogicaBasica/Ex10.java
@@ -1,5 +1,11 @@
 public class Ex10 {
     public static void main(String[] args) {
+        int contador = 5;
+        for (int i = 1; i < 11 ; i++) {
+            System.out.println( contador + " x " + i + " = " + (contador * i));
 
+
+
+        }
     }
 }
