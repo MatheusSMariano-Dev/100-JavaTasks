76f78196050f669baa3bfb5a61bed1ab28e44091
Matheus Mariano <amatheus300@hotmail.com>
2026-01-27 23:54:40 -0300

    Task11

--- 1-LogicaBasica/Ex11.java
+++ 1-LogicaBasica/Ex11.java
@@ -1,5 +1,12 @@
 public class Ex11 {
     public static void main(String[] args) {
+        int numero = 5;
+        long fatorial = 1;
 
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("Fatorial de " + numero + " é: " + fatorial);
     }
 }
