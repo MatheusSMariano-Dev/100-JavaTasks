f94f64f68029febfd26d8a56a44ccad6c020a661
Matheus Mariano <amatheus300@hotmail.com>
2026-01-29 21:07:01 -0300

    task12

--- 1-LogicaBasica/Ex12.java
+++ 1-LogicaBasica/Ex12.java
@@ -1,5 +1,13 @@
 public class Ex12 {
     public static void main(String[] args) {
 
+
+        int n;
+        int soma = 0;
+        for (n = 1; n <= 10; n++) {
+            soma = soma + n;
+
+        }
+        System.out.println("A soma dos números de 1 a 10 é: " + soma);
     }
 }
