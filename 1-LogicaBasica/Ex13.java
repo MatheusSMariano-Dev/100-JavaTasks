a130a0db7f5012cee3718a4e05875eac77960c9f
Matheus Mariano <amatheus300@hotmail.com>
2026-01-31 00:02:57 -0300

    Task 13

--- 1-LogicaBasica/Ex13.java
+++ 1-LogicaBasica/Ex13.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Ex13 {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite a temperatura em Celsius: ");
 
+         double C = leitura.nextDouble();
+         double F = (C * 9/5) + 32;
+
+        System.out.println("A temperatura em Fahrenheit é: " + F);
     }
 }
